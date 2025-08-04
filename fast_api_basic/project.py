0d470193e75de6d8aa75313c5a60d5013b732f80
Loveless Bhartal <lovelessbhartal12@gmail.com>
2025-08-04 15:45:03 +0545

    path and query paramaters

--- fast_api_basic/project.py
+++ fast_api_basic/project.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path , HTTPException ,Query
 import json
 app = FastAPI()
 import os
@@ -23,11 +23,21 @@ def about():
 def view():
     data = load_data()
     return {"patients": data}
-@app.get('/view/{patient_id}')
-def view_patient(patient_id: int):
+#path parameter
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str= Path(..., title="Patient ID", description="The ID of the patient to view")):
+    data=load_data()
+
+    if patient_id in data:
+        return {"patient": data[patient_id]}
+    raise HTTPException(status_code=404, detail="Patient not found")
+#query parameter
+@app.get('/sort')
+def sort_patients(sort_by: str =Query(..., title="Sort By", description="Sort patients by name or age"), order: str = Query("asc", title="Order", description="Sort order, either 'asc' or 'desc'")):
     data = load_data()
-    patient = next((p for p in data if p['id'] == patient_id), None)
-    if patient:
-        return {"patient": patient}
-    else:
-        return {"error": "Patient not found"}
+    if sort_by not in ["name", "age"]:
+        raise HTTPException(status_code=400, detail="Invalid sort parameter")
+    
+    sorted_patients = sorted(data.items(), key=lambda x: x[1][sort_by])
+    return {"sorted_patients": {k: v for k, v in sorted_patients}}
+    
